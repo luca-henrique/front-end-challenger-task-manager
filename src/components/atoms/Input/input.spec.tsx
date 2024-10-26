4f6c6e7918fc84d2a44f5eefcbc9b4d69d57731c
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2024-10-26 18:50:21 -0300

    test: input

--- src/components/atoms/Input/input.spec.tsx
+++ src/components/atoms/Input/input.spec.tsx
@@ -1,6 +1,16 @@
 import { render } from "@testing-library/react"
 import { Input } from "./input"
 
-test('it render', () => {
+describe('it render', () => {
   render(<Input label={""} />)
+
+  test("deve exibir uma mensagem de erro quando 'error' é passado", () => {
+    const { getByText, debug } = render(<Input label="Nome" error="Campo obrigatório" />);
+
+    debug()
+
+    expect(getByText(/Campo obrigatório/)).toBeInTheDocument();
+    expect(getByText(/Campo obrigatório/)).toHaveStyle("color: red");
+  });
+
 })
